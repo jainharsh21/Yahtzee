f7414a7af83be46ab9216785a303ab1c238267ba
HARSH JAIN <jainharsh270@gmail.com>
2020-04-22 12:01:20 +0530

    added roll animation for dice

--- src/Die.js
+++ src/Die.js
@@ -4,6 +4,7 @@ import "./Die.css";
 class Die extends Component {
   static defaultProps = {
     numWords: ["one", "two", "three", "four", "five", "six"],
+    val : 1
   };
 
   constructor(props) {
@@ -16,10 +17,11 @@ class Die extends Component {
   }
 
   render() {
-    const { numWords, locked, val, disabled } = this.props;
+    const { numWords, locked, val, disabled, rolling } = this.props;
 
     let classes = `Die fas fa-dice-${numWords[val - 1]} fa-5x `;
     if (locked) classes += "Die-locked";
+    if(rolling) classes += "Die-rolling";
     return (
       <i className={classes} onClick={this.handleClick} disabled={disabled}></i>
     );
